d6c1c8dddc19d49276311d85c92a94d1dc91bcb8
Fujimoto　Kashin <fujimotokashin0123@gmail.com>
2024-08-05 15:02:56 +0900

    commit

--- src/util/DbConn.java
+++ src/util/DbConn.java
@@ -11,16 +11,22 @@ public class DbConn {
     public static void getConnection(){
 
 
-        String url ="jdbc:mysql://localhost:3306/mydb";
+        //String url ="jdbc:mysql://localhost:3306/mydb";
+        String url ="jdbc:h2:~/test";
         String id = "root";
         String pw = "";
 
         try {
-            Class.forName("com.mysql.jdbc.Driver");
+            //Class.forName("com.mysql.jdbc.Driver");
 
             conn = DriverManager.getConnection(url, id, pw);
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
+
+
+            String createTableSQL = "CREATE TABLE IF NOT EXISTS users (id INT PRIMARY KEY, name VARCHAR(255))";
+
+
+        //} catch (ClassNotFoundException e) {
+         //   e.printStackTrace();
         } catch (SQLException e1) {
             e1.printStackTrace();
         }
